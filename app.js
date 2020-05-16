3ac4826d1e89a89dbda9f45443966eb3c2c3a911
ikram <belhouchet.ikram90@gmail.com>
2020-05-16 02:58:42 +0200

    JWT

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const { MONGOURI } = require("./keys");
+const PORT = 8080;
+
+require("./models/user");
+
+app.use(express.json());
+
+app.use(require("./routes/auth"));
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on(
+  "connected",
+  () => {
+    console.log(
+      "Mongoose default connection is open"
+    );
+  }
+);
+mongoose.connection.on(
+  "error",
+  (err) => {
+    console.log(
+      "error Mongoose connection ",
+      err
+    );
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(
+    "server is running on port ",
+    PORT
+  );
+});
+
+// const CustomMiddelware = (
+//   req,
+//   res,
+//   next
+// ) => {
+//   console.log(
+//     "customMiddleWare executed!"
+//   );
+//   next();
+// };
+// app.use(CustomMiddelware);
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
---
+++ routes/auth.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+  JWT_SECRET,
+} = require("../keys");
+router.get("/protected", (req, res) => {
+  res.send("hello");
+});
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(422).json({
+      error:
+        "email or password is invalid or both!",
+    });
+  }
+  User.findOne({ email: email }).then(
+    (savedUser) => {
+      if (!savedUser) {
+        return res.status(422).json({
+          error:
+            "user doesn't exist  !",
+        });
+      }
+      bcrypt
+        .compare(
+          password,
+          savedUser.password
+        )
+        .then((doMatch) => {
+          if (doMatch) {
+            const token = jwt.sign(
+              { _id: savedUser._id },
+              JWT_SECRET
+            );
+            res.json({ token });
+          } else {
+            return res
+              .status(422)
+              .json({
+                error:
+                  "email or password doesn't match",
+              });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  );
+});
+router.post("/signup", (req, res) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+  if (!email || !password || !name) {
+    res.status(422).json({
+      error:
+        "please add all the fields",
+    });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({
+          error: "user exist already !",
+        });
+      }
+      bcrypt
+        .hash(password, 14)
+        .then((hashedPassword) => {
+          const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+          });
+          user
+            .save()
+            .then((user) => {
+              res.json({
+                message:
+                  "saved with success!",
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+module.exports = router;
